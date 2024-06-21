554d61e611b61413e0f6616241b5ce50037ac5b0
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-06-21 21:54:58 +0530

    My first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+// Adding event listeners :-
+
+// document.getElementById('myButton').addEventListener('click', clickHandle = function () {
+//     alert('Button Clicked!');
+// });
+// document.getElementById('myButton').removeEventListener('click', clickHandle);
+
+// let button = document.getElementById("myButton");
+// button.addEventListener("click", clickHandle = function () {
+//     alert("Button Clicked!");
+// });
+
+
+// // Remove event listeners :-
+
+// button.removeEventListener('click', clickHandle);
+
+
+// Alternative way :-
+
+// function clickHandle() {
+//     alert('Button Clicked!');
+// }
+
+// let button = document.getElementById('myButton');
+// button.addEventListener('click', clickHandle);
+// button.removeEventListener('click', clickHandle);
+
+
+// Event Types :-
+
+// 1. click :-
+
+// let div = document.getElementById('myDiv');
+// div.addEventListener('click', clickHandle = function () {
+//     console.log('mouse clicked!');
+// });
+
+// 2. dbclick :-
+
+// let div = document.getElementById('myDiv');
+// div.addEventListener('dblclick', double = function () {
+//     console.log('double click!');
+// });
+
+// 3. mousedown :-
+
+// let div = document.getElementById('myDiv');
+// div.addEventListener('mousedown', mousedown = function () {
+//     console.log('mouse down!');
+// });
+
+// 4. mouseup :-
+
+// let div = document.getElementById('myDiv');
+// div.addEventListener('mouseup', mouseup = function () {
+//     console.log('mouse up!');
+// });
+
+// 5. mousemove :-
+
+// let div = document.getElementById('myDiv');
+// div.addEventListener('mousemove', mousemove = function () {
+//     console.log('mouse moved!');
+// });
+
+// 6. mouseover :-
+
+// let div = document.getElementById('myDiv');
+// div.addEventListener('mouseover', mouseover = function () {
+//     console.log('mouse over div!');
+// });
+
+// 7. mouseout :-
+
+// let div = document.getElementById('myDiv');
+// div.addEventListener('mouseout', mouseout = function () {
+//     console.log('mouse outside div!');
+// });
+
+// let button = document.getElementById('myButton');
+// button.addEventListener('mouseover', mouseover = function () {
+//     console.log('mouse inside button!');
+// });
+
+// let h2 = document.getElementById('heading');
+// h2.addEventListener('mouseover', mouseover = function () {
+//     console.log('mouse inside heading 2!');
+// });
