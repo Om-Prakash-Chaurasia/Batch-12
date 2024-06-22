3ef5604b66cdf45187472f7ed0002965dd7ab15c
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-06-22 21:35:25 +0530

    2nd commit 22/06/2024

---
+++ form.js
@@ -0,0 +1,42 @@
+// Submit Event :-
+
+// let form = document.getElementById('myForm');
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     let username = document.getElementById('username').value;
+//     let password = document.getElementById('password').value;
+
+//     if (username === '' || password === '') {
+//         alert('Please fill out the  form fields.');
+//     } else {
+//         alert('Form submitted successfully.');
+//     }
+// });
+
+
+
+// Focus Event :-
+
+// let name = document.getElementById('name');
+// name.addEventListener('focus', function () {
+//     console.log('Name input has gained focus');
+// });
+
+// let password = document.getElementById('password');
+// password.addEventListener('focus', function () {
+//     console.log('Password input has gained focus');
+// });
+
+
+// Blur Event :-
+
+// let name = document.getElementById('name');
+// name.addEventListener('blur', function () {
+//     let name = this.value;
+//     if (name === '') {
+//         alert(`Please enter the name`);
+//     } else {
+//         console.log(`Thank you, ${name}`);
+//     }
+// });
