b56722f3de9518329afbbc0e50f14ef915f410b7
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-06-25 21:55:54 +0530

    ES6 features discussed upto Template Literals

---
+++ es6features.js
@@ -0,0 +1,39 @@
+// Traditional / Normal function :-
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// add(5, 2);
+
+// Arrow functions :- Also known as anonymous functions.
+
+// const add = (a, b) => console.log(a + b);
+// add(5, 2);
+
+
+// Other e.g. :-
+
+// const greet = () => 'Hello Everyone!';
+// console.log(greet());
+
+// const square = x => x * x;
+// console.log(square(4));
+
+// Template Literals :-
+
+// let sentence = 'Hi there, how are you?'; // '' -> String literals
+// console.log(sentence);
+
+// let sentences = "Hi, I am fine."; // " " -> String literals
+// console.log(sentences);
+
+// const name = 'Praveen';
+// const greeting = 'Hello, ${name}!';
+// console.log(greeting);
+
+// const greetings = "Hello, ${name}!";
+// console.log(greetings);
+
+// const greet = `Hello, ${name}`; // ` ` -> Template literals
+// console.log(greet);
