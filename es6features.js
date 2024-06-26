07ade0b8c9f97c3ccd5687cbe575e52f8cdffb8c
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-06-26 20:49:01 +0530

    destructuring completed.

--- es6features.js
+++ es6features.js
@@ -37,3 +37,65 @@
 
 // const greet = `Hello, ${name}`; // ` ` -> Template literals
 // console.log(greet);
+
+
+// Destructuring :-
+
+// let arr = [1, 2, 3];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[2]);
+
+// Array destructuring :-
+
+// const [a, b, c, d, e, f, g] = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log(b);
+// console.log(d);
+// console.log(e);
+// console.log(g);
+
+
+// Destructuring with default values :-
+// const [x, y, z = 5] = [1, 2];
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+// Object destructuring :-
+
+// const user = {
+//     name: 'Kamalesh',
+//     age: 26,
+//     state: 'Tamil Nadu',
+//     dist: 'Madurai'
+// };
+
+// // console.log(user.name);
+// // console.log(user.age);
+
+// const { name, age, state, dist } = user;
+// console.log(name);
+// console.log(age);
+// console.log(state);
+// console.log(dist);
+
+
+// Nested Object destructuring :-
+
+// const user = {
+//     name: 'Kamalesh',
+//     address: {
+//         state: 'Tamil Nadu',
+//         city: 'Madurai',
+//         pin: 123456
+//     }
+// };
+
+// const { name, address: { state, city, pin } } = user;
+
+// console.log(name);
+// console.log(state);
+// console.log(city);
+// console.log(pin);
