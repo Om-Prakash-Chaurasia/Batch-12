8eb682be91f4769eec020ab16dd9868b83d44872
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-06-26 21:38:09 +0530

    Spread operator and Rest Parameter covered.

--- es6features.js
+++ es6features.js
@@ -99,3 +99,67 @@
 // console.log(state);
 // console.log(city);
 // console.log(pin);
+
+
+
+
+
+// Spread Operator (...) :- The spread operator (...) allows for expanding iterable elements
+// (such as arrays or objects) into individual elements. It is useful for copying arrays,
+// merging arrays or objects and spreading function arguments.
+
+// Syntax :-
+// const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(array1);
+// const array2 = [4, 5, ...array1];
+// console.log(array2);
+
+// copying :-
+// const original = [1, 2, 3];
+// const copy = [...original]
+// console.log(copy);
+
+// merging :-
+// const arr1 = [1, 2];
+// const arr2 = [3, 4];
+// const merged = [...arr1, ...arr2];
+// console.log(merged);
+
+
+// Spreading in function call :-
+
+// const sum = (a, b, c) => a + b + c;
+// // console.log(sum(1, 2, 3));
+
+// const nums = [1, 2, 3, 4, 5, 6];
+// console.log(sum(...nums));
+
+// Spreading Objects :-
+
+// const obj1 = { a: 1, b: 2 };
+// console.log(obj1);
+// const obj2 = { c: 3, d: 4 };
+// console.log(obj2);
+// const merged = { ...obj1, ...obj2 };
+// console.log(merged);
+
+
+
+// Rest Parameter (...) :- Rest parameters allows a function to accept an indefinite
+// number of arguments as an array. It uses the same (...) syntax as the spread
+// operator, but instead of spreading elements, it collects them.
+
+// Syntax :-
+
+// function sum(...numbers) {
+//     return numbers.reduce((init, curr) => init + curr, 0);
+// }
+
+// console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+// Example 2 :-
+// function concatenate(separator, ...strings) {
+//     return strings.join(separator);
+// }
+
+// console.log(concatenate(', ', 'name', 'age', 'state'));
