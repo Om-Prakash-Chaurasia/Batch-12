62bec29952c365257e8e4c6056a94f609a94eb50
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-07-04 21:13:04 +0530

    Conditional rendering covered.

--- React/batch-12/src/App.jsx
+++ React/batch-12/src/App.jsx
@@ -4,15 +4,20 @@ import Counter from "./components/Counter";
 import JsxExplanation from "./components/JsxExplanation";
 import Props from "./components/Props";
 import EventHandling from "./components/EventHandling";
+import ConditionalRendering from "./components/ConditionalRendering";
+import Mailbox from "./components/ConditionalRendering";
 
 function App() {
+  // const messages = ["Messsage 1", "Message 2", "Message 3"];
   return (
     <div>
       {/* <Greetings name="Om Prakash" />
       <Counter />
       <JsxExplanation /> */}
       {/* <Props name="Deepak" age="25" /> */}
-      <EventHandling />
+      {/* <EventHandling /> */}
+      {/* <ConditionalRendering letMeCheck={true} isLoggedIn={true} /> */}
+      {/* <Mailbox unreadMessages={messages} /> */}
     </div>
   );
 }
---
+++ React/batch-12/src/components/ConditionalRendering.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+// function ConditionalRendering(props) {
+//   if...else :-
+//   const login = props.letMeCheck;
+
+//   if (login) {
+//     return <h1>Welcome back!</h1>;
+//   } else {
+//     return <h1>Please sign in.</h1>;
+//   }
+
+// Elements variable :- Storing elements in variable.
+//   const isLoggedIn = props.isLoggedIn;
+//   let message;
+
+//   if (isLoggedIn) {
+//     message = <h1>Welcome back!</h1>;
+//   } else {
+//     message = <h1>Please sign in.</h1>;
+//   }
+
+//   return <div>{message}</div>;
+// }
+
+// export default ConditionalRendering;
+
+// Inline If with Logical && :-
+// function Mailbox(props) {
+//   const unreadMessages = props.unreadMessages;
+
+//   return (
+//     <div>
+//       <h1>Hello!</h1>
+//       {unreadMessages.length > 2 && (
+//         <h2>You have {unreadMessages.length} unread messages.</h2>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Mailbox;
