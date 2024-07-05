e8955db28f670bd5bdf19466f58ae37b3ee731ef
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-07-05 21:54:08 +0530

    Controlled and Uncontrolled components covered.

--- React/batch-12/src/App.jsx
+++ React/batch-12/src/App.jsx
@@ -7,6 +7,8 @@ import EventHandling from "./components/EventHandling";
 import ConditionalRendering from "./components/ConditionalRendering";
 import Mailbox from "./components/ConditionalRendering";
 import Greetings from "./components/ConditionalRendering";
+import ControlledForm from "./components/ControlledForm";
+import UncontrolledForm from "./components/UncontrolledForm";
 
 function App() {
   // const messages = ["Messsage 1", "Message 2", "Message 3"];
@@ -14,12 +16,14 @@ function App() {
     <div>
       {/* <Greetings name="Om Prakash" /> */}
       {/* <Counter /> */}
-      <JsxExplanation />
+      {/* <JsxExplanation /> */}
       {/* <Props name="Deepak" age="25" /> */}
       {/* <EventHandling /> */}
       {/* <ConditionalRendering letMeCheck={true} isLoggedIn={true} /> */}
       {/* <Mailbox unreadMessages={messages} /> */}
       {/* <Greetings isLoggedIn={true} /> */}
+      {/* <ControlledForm /> */}
+      <UncontrolledForm />
     </div>
   );
 }
---
+++ React/batch-12/src/components/ControlledForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function ControlledForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Name:</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <label>Email:</label>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <label>Password:</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default ControlledForm;
