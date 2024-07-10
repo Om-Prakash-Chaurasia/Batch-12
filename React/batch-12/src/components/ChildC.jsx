895ed357bd3cab497a45c8fb193992cc61c334e0
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-07-10 21:19:30 +0530

    Passing multiple props using Context API covered and It's problem also discussed.

--- React/batch-12/src/components/ChildC.jsx
+++ React/batch-12/src/components/ChildC.jsx
@@ -16,15 +16,44 @@
 
 // Note :- Inside the consumer we can only write a function.
 
+// import React from "react";
+// import { data } from "./Parent";
+
+// function ChildC() {
+//   return (
+//     <div>
+//       <data.Consumer>
+//         {(name) => {
+//           return <h1>My name is {name}.</h1>;
+//         }}
+//       </data.Consumer>
+//     </div>
+//   );
+// }
+
+// export default ChildC;
+
+// Passing multiple props :-
+
 import React from "react";
-import { data } from "./Parent";
+import { data, data1 } from "./Parent";
 
 function ChildC() {
   return (
     <div>
       <data.Consumer>
         {(name) => {
-          return <h1>My name is {name}.</h1>;
+          return (
+            <data1.Consumer>
+              {(age) => {
+                return (
+                  <h1>
+                    My name is {name} and I am {age} years old.
+                  </h1>
+                );
+              }}
+            </data1.Consumer>
+          );
         }}
       </data.Consumer>
     </div>
--- React/batch-12/src/components/Parent.jsx
+++ React/batch-12/src/components/Parent.jsx
@@ -17,24 +17,50 @@
 
 // Context API :- create, Provider, and Consumer.
 
+// import React, { createContext } from "react";
+// import ChildA from "./ChildA";
+
+// const data = createContext();
+
+// function Parent() {
+//   const name = "Om Prakash";
+
+//   return (
+//     <div>
+//       <data.Provider value={name}>
+//         <ChildA />
+//       </data.Provider>
+//     </div>
+//   );
+// }
+
+// export default Parent;
+// export { data };
+
+// Is context API problematic? => Yes, it is. Lets see how.
+
 import React, { createContext } from "react";
 import ChildA from "./ChildA";
 
 const data = createContext();
+const data1 = createContext();
 
 function Parent() {
   const name = "Om Prakash";
+  const age = 24;
 
   return (
     <div>
       <data.Provider value={name}>
-        <ChildA />
+        <data1.Provider value={age}>
+          <ChildA />
+        </data1.Provider>
       </data.Provider>
     </div>
   );
 }
 
 export default Parent;
-export { data };
+export { data, data1 };
 
-// Is context API problematic? => Yes, it is. Lets see how.
+// // useContext Hook :- create, Provider, and useContext hook.
