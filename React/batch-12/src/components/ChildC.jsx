d370e0c4d5e21adba12784dc18a69c26d1af0dd7
Om Prakash Chaurasia <jackadvic07@gmail.com>
2024-07-10 21:42:05 +0530

    useContext Hook covered.

--- React/batch-12/src/components/ChildC.jsx
+++ React/batch-12/src/components/ChildC.jsx
@@ -35,27 +35,64 @@
 
 // Passing multiple props :-
 
-import React from "react";
+// import React from "react";
+// import { data, data1 } from "./Parent";
+
+// function ChildC() {
+//   return (
+//     <div>
+//       <data.Consumer>
+//         {(name) => {
+//           return (
+//             <data1.Consumer>
+//               {(age) => {
+//                 return (
+//                   <h1>
+//                     My name is {name} and I am {age} years old.
+//                   </h1>
+//                 );
+//               }}
+//             </data1.Consumer>
+//           );
+//         }}
+//       </data.Consumer>
+//     </div>
+//   );
+// }
+
+// export default ChildC;
+
+// useContext Hook :-
+
+// import React, { useContext } from "react";
+// import { data } from "./Parent";
+
+// function ChildC() {
+//   const name = useContext(data);
+
+//   return (
+//     <div>
+//       <h1>Hi! my name is {name}.</h1>
+//     </div>
+//   );
+// }
+
+// export default ChildC;
+
+// Passing multiple datas :-
+
+import React, { useContext } from "react";
 import { data, data1 } from "./Parent";
 
 function ChildC() {
+  const name = useContext(data);
+  const age = useContext(data1);
+
   return (
     <div>
-      <data.Consumer>
-        {(name) => {
-          return (
-            <data1.Consumer>
-              {(age) => {
-                return (
-                  <h1>
-                    My name is {name} and I am {age} years old.
-                  </h1>
-                );
-              }}
-            </data1.Consumer>
-          );
-        }}
-      </data.Consumer>
+      <h1>
+        My name is {name} and my age is {age}.
+      </h1>
     </div>
   );
 }
--- React/batch-12/src/components/Parent.jsx
+++ React/batch-12/src/components/Parent.jsx
@@ -39,6 +39,54 @@
 
 // Is context API problematic? => Yes, it is. Lets see how.
 
+// import React, { createContext } from "react";
+// import ChildA from "./ChildA";
+
+// const data = createContext();
+// const data1 = createContext();
+
+// function Parent() {
+//   const name = "Om Prakash";
+//   const age = 24;
+
+//   return (
+//     <div>
+//       <data.Provider value={name}>
+//         <data1.Provider value={age}>
+//           <ChildA />
+//         </data1.Provider>
+//       </data.Provider>
+//     </div>
+//   );
+// }
+
+// export default Parent;
+// export { data, data1 };
+
+// useContext Hook :- create, Provider, and useContext hook.
+
+// import React, { createContext } from "react";
+// import ChildA from "./ChildA";
+
+// const data = createContext();
+
+// function Parent() {
+//   const name = "Om Prakash";
+
+//   return (
+//     <div>
+//       <data.Provider value={name}>
+//         <ChildA />
+//       </data.Provider>
+//     </div>
+//   );
+// }
+
+// export default Parent;
+// export { data };
+
+// Passing multiple datas :-
+
 import React, { createContext } from "react";
 import ChildA from "./ChildA";
 
@@ -62,5 +110,3 @@ function Parent() {
 
 export default Parent;
 export { data, data1 };
-
-// // useContext Hook :- create, Provider, and useContext hook.
